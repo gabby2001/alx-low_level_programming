07cd0082a94ae5097aa119a1731e0383d9a62b69
gabby2001 <gabrielplus2001@gmail.com>
2022-06-07 18:48:24 +0000

    commited

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,17 @@
-<<<<<<< HEAD
 #ifndef _MY_HEADER_H_
 #define _MY_HEADER_H_
-=======
 #include "main.h"
->>>>>>> 1aed404ab7bb442f521184d4ef6c7e3a2a474690
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-<<<<<<< HEAD
 int main (void)
 {
 #include <stdio.h>
-=======
 int main(void)
 {
->>>>>>> 1aed404ab7bb442f521184d4ef6c7e3a2a474690
 		char *sh = "_putchar";
 
 			while (*sh)
@@ -28,7 +23,4 @@ int main(void)
 
 					return (0);
 }
-<<<<<<< HEAD
 #endif /* _MY_HEADER_H_ */
-=======
->>>>>>> 1aed404ab7bb442f521184d4ef6c7e3a2a474690
