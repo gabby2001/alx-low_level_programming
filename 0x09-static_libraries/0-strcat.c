df1688f361203126c2893a0d195e448e262b55aa
gabby2001 <gabrielplus2001@gmail.com>
2022-06-24 18:29:23 +0000

    added

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings
+ * @dest: input parameter string
+ * @src: input parameter string
+ *
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)~
