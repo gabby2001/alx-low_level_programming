ec5719c16f5f0489439d52de2da6443a9974a20a
gabby2001 <gabrielplus2001@gmail.com>
2022-06-26 05:54:14 +0000

    Create a script called create_static_lib.sh that creates a static library called liball.a from all the .c files that are in the current directory.

--- 0x09-static_libraries/0-strcat.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-/**
- * _strcat - concatenates two strings
- * @dest: input parameter string
- * @src: input parameter string
- *
- * Return: dest
- */
-char *_strcat(char *dest, char *src)~
